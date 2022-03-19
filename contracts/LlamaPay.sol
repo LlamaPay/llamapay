78bf25ebabf16365b65e85b3818c4df12a228b1a
0xngmi <0xngmi@protonmail.com>
2022-03-19 01:48:10 +0000

    remove console

--- contracts/LlamaPay.sol
+++ contracts/LlamaPay.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 
 interface Factory {
     function owner() external returns (address);
--- contracts/LlamaPayFactory.sol
+++ contracts/LlamaPayFactory.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: None
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./LlamaPay.sol";
 
